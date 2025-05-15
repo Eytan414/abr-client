b56184529e959bafb28e2f7f7064dff6002fcd7b
eytan414 <eytanivri@gmail.com>
2025-05-15 20:43:00 +0300

    optimize bg img loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,34 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { environment } from '../environments/environment';
+import { Observable } from 'rxjs/internal/Observable';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    AsyncPipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'tomer';
+  bgUrl = `${environment.apiUrl}webp-image/bg`;
   bgLoaded = signal<boolean>(false);
 
-  getBgString() {
-    return `${environment.apiUrl}webp-image/bg`
-  }
+  readonly preloadedUrl$: Observable<string> = new Observable(observer => {
+    const img = new Image();
+    img.onload = () => {
+      observer.next(this.bgUrl);
+      observer.complete();
+      this.bgLoaded.set(true);
+    };
+    img.src = this.bgUrl;
+  });
+
+
 }
