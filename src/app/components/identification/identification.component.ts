ea58a22a7330015944275d03c88022dd67fb3942
eytan414 <eytanivri@gmail.com>
2025-05-19 20:44:27 +0300

    initial impl of checking phone as soon as entered

--- src/app/components/identification/identification.component.ts
+++ src/app/components/identification/identification.component.ts
@@ -1,13 +1,17 @@
-import { ChangeDetectionStrategy, Component, computed, inject, Injector, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, inject, Injector, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { BackendService } from '../../services/backend.service';
-import { tap } from 'rxjs/operators';
+import { finalize, tap } from 'rxjs/operators';
 import { AppService } from '../../services/app.service';
 import { FormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'identification',
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './identification.component.html',
   styleUrl: './identification.component.scss',
   standalone: true,
@@ -22,10 +26,55 @@ export class IdentificationComponent implements OnInit {
   grade = signal<string>('');
   phone = signal<string>('');
   school = signal<string>('');
+  password = signal<string>('');
+  isMemberFlow = signal<boolean>(false);
+  loading = signal<boolean>(false);
 
+  constructor() {
+    effect(() => {
+      const val = this.phone();
+      if (/^\d{10}$/.test(val)) {
+        this.loading.set(true);
+        this.backend.checkIsSuper(this.phone()).pipe(
+          tap((s) => {
+            const schoolId = s.supervisor?.schoolId;
+            if(schoolId) {
+              this.appService.userDetails.update(u => ({ ...u, schoolId }) );
+              this.isMemberFlow.set(true);
+            }
+          }),
+          finalize(() => {debugger; this.loading.set(false)})
+        ).subscribe();
+      } else {
+        this.isMemberFlow.set(false);
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.backend.getSchoolList().subscribe();
+  }
+
+  login() {
+    this.backend.login(this.password()).subscribe();
+  }
+  checkIfSupervisor() {
+    if (this.isMemberFlow()) {
+      this.login();
+      return;
+    }
+    this.backend.checkIsSuper(this.phone()).pipe(
+      tap((result) => {
+        if (result.supervisor) {
+          this.updateUserDetails(result.supervisor.schoolId);
+          this.router.navigateByUrl('/manage');
+        }
+        else {
+          this.updateUserDetails();
+          this.gotoInstructions();
+        }
+      })
+    ).subscribe();
   }
 
   updateUserDetails(schoolId: string = this.school()) {
@@ -38,21 +87,12 @@ export class IdentificationComponent implements OnInit {
     }));
   }
 
-  checkIfSupervisor() {
-    this.backend.checkIsSuper(this.phone()).pipe(
-      tap((result) => {
-        if (result.supervisor) {
-          this.updateUserDetails(result.supervisor.schoolId);
-          this.router.navigateByUrl('/login');
-        }
-        else {
-          this.updateUserDetails();
-          this.gotoInstructions();
-        }
-      })
-    ).subscribe();
-  }
   gotoInstructions() {
     this.router.navigateByUrl('/instructions');
+  }
+
+  onInputChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.phone.set(value);
   }
 }
