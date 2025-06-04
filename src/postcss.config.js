0d8a0604129877bc81fa9c4120030bcdce2bc1cb
eytan414 <eytanivri@gmail.com>
2025-06-05 00:34:35 +0300

    optimize bg loading - wip

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,5 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+	content: [
+		'./src/**/*.html',
+		'./src/**/*.ts',
+	],
+	safelist: [],
+	defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+
 module.exports = {
 	plugins: [
+		purgecss,
+
 		require('cssnano')({
 			preset: 'default',
 		}),
