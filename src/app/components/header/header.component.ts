d1d45a7eeb1dfa9bbfb342032fe02980a19935b2
eytan414 <eytanivri@gmail.com>
2025-06-05 00:57:05 +0300

    optimize loading times for lcp

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,26 +1,16 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { RouterModule } from '@angular/router';
-import { Observable } from 'rxjs/internal/Observable';
 import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, AsyncPipe],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
-  apiUrl = environment.apiUrl;
-  logoLoaded = signal<boolean>(false);
-  readonly preloadedLogoUrl$: Observable<string> = new Observable(observer => {
-    const img = new Image();
-    img.onload = () => {
-      observer.next(this.apiUrl + 'webp-image/logo');
-      observer.complete();
-      this.logoLoaded.set(true);
-    };
-    img.src = this.apiUrl + 'webp-image/logo';
-  });
+  logoUrl = `${environment.apiUrl}webp-image/logo`;
+  whatsappUrl = `${environment.apiUrl}webp-image/whatsapp`;  
 }
